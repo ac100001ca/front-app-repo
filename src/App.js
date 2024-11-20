ce2addbed90514d4e4cd753d6b25ed14bdf7b795
ac100001ca <ac100001ca@gmail.coom>
2024-11-20 07:39:31 +0000

    update app.js

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog 2
+            EKS DEMO Blog 4
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
